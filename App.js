d7a838d82cd22902685130a0253fe5a14847df4c
Tabare Bermudez <tabarebermudez@gmail.com>
2023-04-20 12:56:55 -0300

    [New app]

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Text, View, Button, TextInput, FlatList, TouchableOpacity, Modal } from 'react-native'
+import { styles } from './styles'
 
 export default function App() {
+
+  const [eventText, setEventText] = useState('')
+  const [eventList, setEventList] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [selectedElement, setSelectedElement] = useState(null)
+
+
+  const onAddEvent = () => {
+    if (eventText.length === 0) return
+    setEventList([
+      ...eventList,
+      {
+        id: Math.random().toString(),
+        value: eventText
+      }
+    ])
+    setEventText('')
+  }
+
+  const onHandleEvent = (id) => {
+    setModalVisible(!modalVisible)
+    const selectedEvent = eventList.find(event => event.id === id)
+    setSelectedElement(selectedEvent)
+  }
+
+  const onHandleCancelModal = () => {
+    setModalVisible(!modalVisible)
+    setSelectedElement(null)
+  }
+
+  const onHandleDeleteModal = (id) => {
+    setEventList(eventList.filter(event => event.id != id))
+    setModalVisible(!modalVisible)
+    setSelectedElement(null)
+  }
+
+  const renderItem =({ item }) => (
+    <TouchableOpacity key={item.id} style={styles.itemContainer} onPress={() => onHandleEvent(item.id)}>
+      <Text style={styles.item}>{item.value}</Text>
+    </TouchableOpacity>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Enter your event'
+         style={styles.input}
+         value={eventText}
+         onChangeText={(eventText) => setEventText(eventText)}
+         />
+        <Button title='Add' color='#52528C' onPress={onAddEvent} />
+      </View>
+      
+      <View style={styles.listContainer}>
+        <FlatList
+          renderItem={renderItem}
+          data={eventList}
+          keyExtractor={(item) => item.id}
+          alwaysBounceVertical={false}
+        />
+      </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Modal visible={modalVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task Detail</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>Are you sure to delete this item?</Text>
+            <Text style={styles.modalDetailEvent}>{selectedElement?.value}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              title='Cancel'
+              color='#52528C'
+              onPress={() => onHandleCancelModal()}
+            />
+            <Button
+              title='Delete'
+              color='#52528C'
+              onPress={() => onHandleDeleteModal(selectedElement.id)}
+            />
+          </View>
+        </View>
+      </Modal>
+
+    </View>
+  )
+}
